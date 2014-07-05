c3dab3901f253fd484e516d42eb38777efdc42aa
Kyle Hargraves <pd@krh.me>
2014-07-05 17:01:15 -0500

    Support `fit` by defaulting to filter on `:focus`

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,6 +34,10 @@ RSpec.configure do |config|
   config.backtrace_exclusion_patterns << /vendor\//
   config.backtrace_exclusion_patterns << /lib\/rspec\/rails/
 
+  # Use `fit`, `fcontext`, etc. to focus on particular examples.
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
