58a11b6ee096f02d20f48952ef15d793ff5a465e
Kyle Hargraves <pd@krh.me>
2014-07-05 18:27:52 -0500

    Appraisals over rails 3.2.x, 4.0.x, 4.1.x

--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -11,4 +11,9 @@ Dummy::Application.configure do
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
+  config.eager_load_paths = []
+
+  if config.active_support.respond_to?(:deprecation=)
+    config.active_support.deprecation = :stderr
+  end
 end
